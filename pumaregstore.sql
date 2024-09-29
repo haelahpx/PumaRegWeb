d2b1dbd9683d8cb2ff83dcd620633251387d89c3
Muhammad Haikal Islami <143589943+haelahpx@users.noreply.github.com>
2024-09-29 21:26:44 +0700

    Update pumaregstore.sql

--- pumaregstore.sql
+++ pumaregstore.sql
@@ -1,42 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Sep 29, 2024 at 01:41 PM
--- Server version: 8.0.30
--- PHP Version: 8.1.10
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE category (
+  category_id int NOT NULL,
+  categoryname varchar(100) NOT NULL
+) ENGINE=InnoDB;
 
 --
--- Database: `pumaregstore`
+-- Dumping data for table category
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `category`
---
-
-CREATE TABLE `category` (
-  `category_id` int NOT NULL,
-  `categoryname` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `category`
---
-
-INSERT INTO `category` (`category_id`, `categoryname`) VALUES
+INSERT INTO category (category_id, categoryname) VALUES
 (18, 'hoodie'),
 (19, 'T-Shirt'),
 (20, 'Shoes'),
@@ -47,51 +18,51 @@ INSERT INTO `category` (`category_id`, `categoryname`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Table structure for table product
 --
 
-CREATE TABLE `product` (
-  `product_id` int NOT NULL,
-  `productname` varchar(255) NOT NULL,
-  `category_id` int NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `price` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE product (
+  product_id int NOT NULL,
+  productname varchar(255) NOT NULL,
+  category_id int NOT NULL,
+  image varchar(255) NOT NULL,
+  description varchar(255) NOT NULL,
+  price decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB;
 
 --
--- Dumping data for table `product`
+-- Dumping data for table product
 --
 
-INSERT INTO `product` (`product_id`, `productname`, `category_id`, `image`, `description`, `price`) VALUES
+INSERT INTO product (product_id, productname, category_id, image, description, price) VALUES
 (10, 'Black Hoodie', 18, '66f90ba871923-Untitled.png', 'good hoodie', 100000.00),
 (11, 'Black T-Shirt', 19, '66f90bbf10deb-3-scaled.png', 'oversized ', 60000.00),
 (12, 'Black Jacket', 23, '66f90be1ca69c-98047-19VM_F.png', 'cool', 200000.00),
 (13, 'Manchester United Jersey', 21, '66f90c03e0307-2ecb1db2b03578d95b80af97cc69a9d3.jpg', 'Best Team ITW', 150000.00),
 (14, 'Casual Shoes', 20, '66f90c28b0d2f-Sepatu-Sneaker-CA-Pro-OW.png', 'normal', 100000.00),
 (15, 'Black Pants', 22, '66f90c48df356-pamt.png', 'is it black?', 75000.00),
-(16, 'Jacket', 23, '66f90c74ae822-jaketgunung.jpeg', 'dont  know', 300000.00),
+(16, 'Jacket', 23, '66f90c74ae822-jaketgunung.jpeg', 'dont know', 300000.00),
 (17, 'steve', 18, '66f90ca223e36-Steve.jpg', 'dk', 1000000.00);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table users
 --
 
-CREATE TABLE `users` (
-  `user_id` int NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `user_type` enum('admin','user') NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE users (
+  user_id int NOT NULL,
+  username varchar(50) NOT NULL,
+  email varchar(200) NOT NULL,
+  password varchar(255) NOT NULL,
+  user_type enum('admin','user') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table users
 --
 
-INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `user_type`) VALUES
+INSERT INTO users (user_id, username, email, password, user_type) VALUES
 (8, 'admin', 'admin@gmail.com', '$2y$10$aOP.SJ.dSzHlApYD./UK.u8nLCa7WX63bIrGvzdsisjHc8npcEg1W', 'admin'),
 (9, 'test', 'test@gmail.com', '$2y$10$rtS3xAO4dMz.v.BRDkUi6OVbeg44nbGAxQrQgM7WA/FNRgvwhL/ka', 'user');
 
@@ -100,57 +71,53 @@ INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `user_type`) VA
 --
 
 --
--- Indexes for table `category`
+-- Indexes for table category
 --
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`category_id`);
+ALTER TABLE category
+  ADD PRIMARY KEY (category_id);
 
 --
--- Indexes for table `product`
+-- Indexes for table product
 --
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`),
-  ADD KEY `fk_ct` (`category_id`);
+ALTER TABLE product
+  ADD PRIMARY KEY (product_id),
+  ADD KEY fk_ct (category_id);
 
 --
--- Indexes for table `users`
+-- Indexes for table users
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+ALTER TABLE users
+  ADD PRIMARY KEY (user_id);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `category`
+-- AUTO_INCREMENT for table category
 --
-ALTER TABLE `category`
-  MODIFY `category_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+ALTER TABLE category
+  MODIFY category_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT for table `product`
+-- AUTO_INCREMENT for table product
 --
-ALTER TABLE `product`
-  MODIFY `product_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+ALTER TABLE product
+  MODIFY product_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table users
 --
-ALTER TABLE `users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE users
+  MODIFY user_id int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `product`
+-- Constraints for table product
 --
-ALTER TABLE `product`
-  ADD CONSTRAINT `fk_ct` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-COMMIT;
+ALTER TABLE product
+  ADD CONSTRAINT fk_ct FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
